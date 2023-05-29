6dd02733ed436a14e581bb65f3590f849dc5c6f9
kellyzhangg <kellyzhangg12@gmail.com>
2023-05-29 17:51:21 -0500

    Updates

---
+++ precache-manifest.858979111aab20e02303f51a6ddd8ec6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9d569fe9eb4610c5f06",
+    "url": "/portfolio/static/css/main.6f884283.chunk.css"
+  },
+  {
+    "revision": "f9d569fe9eb4610c5f06",
+    "url": "/portfolio/static/js/main.f9d569fe.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/portfolio/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/portfolio/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "95c168070371839375c3",
+    "url": "/portfolio/static/js/runtime~main.95c16807.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2dbe4cc393f6aba75defefe63df8cf9b",
+    "url": "/portfolio/index.html"
+  }
+];
