a3de22e4fab233433b79e31fee1698f656cb6591
kellyzhangg <kellyzhangg12@gmail.com>
2023-05-30 16:15:40 -0500

    check

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          HELLO!!!
+          HELLO...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
